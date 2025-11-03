4cdcb68b1e7d9db14f1c6c948a1d9cb4628149ed
Liru <rlizun005@gmail.com>
2025-11-04 00:03:37 +0200

    PlayerChanges
    Player movement system changed and player sprites changed

--- Assets/Scripts/GameScripts/PlayerMovement.cs
+++ Assets/Scripts/GameScripts/PlayerMovement.cs
@@ -1,44 +1,37 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
-{ //Пересування гравця
+{ //РџРµСЂРµСЃСѓРІР°РЅРЅСЏ РіСЂР°РІС†СЏ
 
-    private float speed = 2;
-    private Vector3 directon;
+    private float speed = 2f;
+    private Vector2 direction;
+    private Rigidbody2D rb;
+    private Animator animator;
 
-    public Animator animator;
-
-    private void Update()
+    void Start()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        directon = new Vector3(horizontal, vertical);
-
-        AnimateMovement(directon);       
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();      
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
-        this.transform.position += directon * speed * Time.deltaTime;
+        rb.linearVelocity = direction * speed;
     }
 
-
-
-    //Передача значень для аніматора
-    void AnimateMovement(Vector3 direction)
+    public void Move(InputAction.CallbackContext context)
     {
-        if(direction.magnitude > 0)//Гравець пересувається
-        {
-            animator.SetBool("isMoving", true);
-            animator.SetFloat("horizontal", direction.x);
-            animator.SetFloat("vertical", direction.y);
-        }
-        else//гравець не пересувається
+        animator.SetBool("isMoving", true);
+
+        if (context.canceled)
         {
             animator.SetBool("isMoving", false);
+            animator.SetFloat("lastHorizontal", direction.x);
+            animator.SetFloat("lastVertical", direction.y);
         }
+        direction = context.ReadValue<Vector2>();
+        animator.SetFloat("horizontal", direction.x);
+        animator.SetFloat("vertical", direction.y);
     }
-
-
 }
